c5979877a1f7e7a4cbe128b17469db82267bdd27
maheee7 <mahendran.s746@gmail.com>
2026-04-01 16:39:17 +0530

    updated the package.json

--- cab-backend/src/config/db.ts
+++ cab-backend/src/config/db.ts
@@ -1,10 +1,14 @@
+
 import mysql from 'mysql2/promise';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const databaseConfig = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Mahe@123',
-  database: 'cab',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
